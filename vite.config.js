01e784de262779286905c3801ad8afcea8f08e78
Jules Pasquier <julespasquier55@gmail.com>
2025-11-28 18:51:28 +0100

    Ajout du fichier vercel.json pour éviter les 404

--- vite.config.js
+++ vite.config.js
@@ -9,10 +9,6 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
-  server: {
-    port: 3000,
-    open: true
-  },
   build: {
     outDir: 'dist',
     sourcemap: true,
